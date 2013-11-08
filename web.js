65b3832c1da997969b2e5f2fa35a59f2b24ec2a1
Munair <munair@gmail.com>
2013-11-08 10:35:07 +0000

    testing form usage.

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var postmark = require("postmark")(process.env.POSTMARK_API_KEY);
+var postmark = require('postmark')(process.env.POSTMARK_API_KEY);
 var swig = require('swig');
 
 var app = express(express.logger());
@@ -8,17 +8,17 @@ var app = express(express.logger());
 app.use(express.bodyParser());
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
-app.set('contact form', 'initializing value');
+app.set('form subject', 'initializing value');
 app.set('views', __dirname + '/');
 
 
 app.get('/', function(request, response) {
-  app.set('contact form', 'index');
+  app.set('form subject', 'Free Class Request');
   response.render('index', { });
 });
 
 app.get('/meditation', function(request, response) {
-  app.set('contact form', 'meditation');
+  app.set('form subject', 'Reservation for Capoeira + Meditation');
   response.render('meditation', { });
 });
 
@@ -26,19 +26,18 @@ app.post('/contact', function(request, response) {
   var name = request.body.name;
   var email = request.body.email;
   var mobile = request.body.mobile;
-  var out = "contact name: " + name + "\ncontact email: " + email + "\nmobile: " + mobile + "\ncontact form used: " + app.get('contact form') + "\n";
+  var out = 'contact name: ' + name + '\ncontact email: ' + email + '\nmobile: ' + mobile + '\n';
   postmark.send({
-    "From": "zumbi@cdoseoul.com",
-    "To": "zumbi@cdoseoul.com",
-    "Subject": "Free Class Signup Form Submission",
-    "TextBody": out,
-    "Tag": "registrant"
+    'From': 'zumbi@cdoseoul.com',
+    'To': 'zumbi@cdoseoul.com',
+    'Subject': app.get('form subject'),
+    'TextBody': out,
   }, function(error, success) {
        if(error) {
-          console.error("Unable to send via postmark: " + error.message);
+          console.error('Unable to send via postmark: ' + error.message);
          return;
        }
-    console.info("Sent to postmark for delivery")
+    console.info('Sent to postmark for delivery')
   });
 
   response.redirect('back');
@@ -46,5 +45,5 @@ app.post('/contact', function(request, response) {
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
